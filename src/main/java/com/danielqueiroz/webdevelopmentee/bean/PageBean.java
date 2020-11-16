4787f702fc00ecc3c9c5c0dd4a5c7087e5568f95
queirozdaniel <danielqueirozak77@gmail.com>
2020-11-15 23:08:44 -0300

    Usando o Redirect e recuperando escopo com Flash

--- src/main/java/com/danielqueiroz/webdevelopmentee/bean/PageBean.java
+++ src/main/java/com/danielqueiroz/webdevelopmentee/bean/PageBean.java
@@ -1,20 +1,33 @@
 package com.danielqueiroz.webdevelopmentee.bean;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 @RequestScoped
 public class PageBean {
 
-    private String page;
+    @Inject
+    private Flash flash;
 
-    public String goTo(){
-        if ("b".equals(page)){
+    private String page;
+    private String name;
+
+    public String goTo() {
+        if ("b".equals(page)) {
             return "page_two";
         } else {
             return "index";
         }
+    }
+
+    public String processar(){
+        //Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+        flash.put("nomeDoUsuario",name);
+
+        return "result?faces-redirect=true";
     }
 
     public String getPage() {
@@ -23,5 +36,13 @@ public class PageBean {
 
     public void setPage(String page) {
         this.page = page;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
