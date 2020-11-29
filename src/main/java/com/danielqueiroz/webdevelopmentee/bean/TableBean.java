2dc0ccbee9af4ac22b6e3128b02555c5d0b293af
queirozdaniel <danielqueirozak77@gmail.com>
2020-11-29 17:23:04 -0300

    Modificando criação de tabela para usar ui:repeat

--- src/main/java/com/danielqueiroz/webdevelopmentee/bean/TableBean.java
+++ src/main/java/com/danielqueiroz/webdevelopmentee/bean/TableBean.java
@@ -3,11 +3,8 @@ package com.danielqueiroz.webdevelopmentee.bean;
 import com.danielqueiroz.webdevelopmentee.model.Despesa;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.model.ListDataModel;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,47 +12,31 @@ import java.util.List;
 @SessionScoped
 public class TableBean implements Serializable {
 
-    private List<Despesa> despesaList = new ArrayList<>();
-    private ListDataModel<Despesa> despesas = new ListDataModel<>(despesaList);
+    private List<Despesa> despesas = new ArrayList<>();
 
-    public TableBean() {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Despesa d = new Despesa();
-        try {
-            d.setData(sdf.parse("15/02/2015"));
-            d.setDescricao("Item desconhecido");
-            d.setEdit(false);
-            d.setValor(129.0);
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        despesaList.add(d);
-    }
-
-    public String inserir(){
+    public String inserir() {
         Despesa despesa = new Despesa();
         despesa.setEdit(true);
-        despesaList.add(despesa);
+        despesas.add(despesa);
         return "despesas";
     }
 
-    public String editar(Despesa despesa){
+    public String editar(Despesa despesa) {
         despesa.setEdit(true);
         return null;
     }
 
-    public String gravar(Despesa despesa){
+    public String gravar(Despesa despesa) {
         despesa.setEdit(false);
         return null;
     }
 
-    public String excluir(Despesa despesa){
-        despesaList.remove(despesa);
+    public String excluir(Despesa despesa) {
+        despesas.remove(despesa);
         return null;
     }
 
-    public ListDataModel<Despesa> getDespesas() {
+    public List<Despesa> getDespesas() {
         return despesas;
     }
 }
