261497eeb95121ab439d053cb3409a11ba2b2109
queirozdaniel <danielqueirozak77@gmail.com>
2020-12-17 23:21:13 -0300

    Usando EJB Singleton

---
+++ src/main/java/com/danielqueiroz/webdevelopmentee/bean/ejb/FormItemSingletonBean.java
@@ -0,0 +1,24 @@
+package com.danielqueiroz.webdevelopmentee.bean.ejb;
+
+
+import com.danielqueiroz.webdevelopmentee.ejb.ItensSingletonBean;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named("formItemS")
+@RequestScoped
+public class FormItemSingletonBean implements Serializable {
+
+    @EJB
+    private ItensSingletonBean itensSingletonBean;
+
+    public List<String> getItens(){
+        return itensSingletonBean.getItens();
+    }
+
+}
---
+++ src/main/java/com/danielqueiroz/webdevelopmentee/ejb/ItensSingletonBean.java
@@ -0,0 +1,24 @@
+package com.danielqueiroz.webdevelopmentee.ejb;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.util.Arrays;
+import java.util.List;
+
+@Singleton
+@Startup // Inicia junto ao servidor
+public class ItensSingletonBean {
+
+    private List<String> itens;
+
+    @PostConstruct
+    private void init(){
+        itens = Arrays.asList("Arroz", "Feijão", "Café", "Macarrão");
+        System.out.println("Inicializando itens");
+    }
+
+    public List<String> getItens() {
+        return itens;
+    }
+}
