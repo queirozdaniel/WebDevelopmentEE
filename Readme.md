5155d5ce9227fc73f8a0355c591241d96bc4d1bf
Daniel Queiroz <queirozdaniel>
2021-01-10 22:03:55 -0300

    Usando HttpSessionBindingListener

--- Readme.md
+++ Readme.md
@@ -7,8 +7,15 @@
 - Branch projeto: Projeto sendo implementado
 ---
 
-
-
+Configurações do projeto:
+- WildFly na versão 19.+
+- Arquivo de inicialização é o standalone-full.xml
+- Em standalone-full.xml, é necessáio ter linhas dentro de subsystem messaging-activemq:
+~~~~
+ <jms-queue name="SoftQueue" entries="java:/jms/queue/SoftQueue"/>
+ <jms-topic name="SoftTopic" entries="java:/jms/topic/SoftTopic"/>   
+~~~~
+- Necessáio ter conexão com DB configurada
 
 Vamos codar ?  🎉🙌
 
--- src/main/java/com/danielqueiroz/webdevelopmentee/servlet/MessageServlet.java
+++ src/main/java/com/danielqueiroz/webdevelopmentee/servlet/MessageServlet.java
@@ -1,5 +1,7 @@
 package com.danielqueiroz.webdevelopmentee.servlet;
 
+import com.danielqueiroz.webdevelopmentee.model.Produto;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,10 +16,22 @@ public class MessageServlet extends HttpServlet {
 
         response.setContentType("text/html");
 
+        Produto x = new Produto();
+        Produto y = new Produto();
+
+        HttpSession session = request.getSession();
+        session.setAttribute("p1", x);
+        session.setAttribute("p2", y);
+
+
         try(PrintWriter out = response.getWriter()){
             out.print("<html><body>");
             out.print("<h1>Primerio Servlet<h1/>");
             out.print("<h3>Está funcionando...<h3/>");
+
+            out.println("<p>Produto 1: " + x.getNome() +"</p>");
+            out.println("<p>Produto 2: " + y.getNome() +"</p>");
+
             out.print("<body/><html/>");
         }
 
